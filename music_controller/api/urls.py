9e29470b611238e8dc796c464b38974af24b7f18
Arafat Zaman Ratul <133839523+ratul41907@users.noreply.github.com>
2025-06-10 07:46:41 +0600

    Add files via upload

---
+++ music_controller/api/urls.py
@@ -0,0 +1,9 @@
+#from django.contrib import admin
+from django.urls import path
+from .views import main 
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('home', main),
+    path('',main)
+]
