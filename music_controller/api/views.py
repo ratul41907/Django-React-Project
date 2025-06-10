c16acd1d54d6428421894c3a91ec587293f4f0f2
Arafat Zaman Ratul <133839523+ratul41907@users.noreply.github.com>
2025-06-10 10:16:27 +0600

    Add files via upload

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer  # Assuming RoomSerializer is imported from serializers.py
+from .models import Room  # Assuming Room is imported from models.py
+class RoomView(generics.CreateAPIView):
+    # This view will list all rooms
+    queryset = Room.objects.all()  # Assuming Room is imported from models.py
+    serializer_class = RoomSerializer  # Assuming RoomSerializer is imported from serializers.py
+#from django.http import HttpResponse
 # Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+##   return HttpResponse("<h1>Hello</h1>")
