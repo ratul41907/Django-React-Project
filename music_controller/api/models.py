dbc79f97c739f42b32248c01a7d7865f083d807c
Arafat Zaman Ratul <133839523+ratul41907@users.noreply.github.com>
2025-06-10 09:56:03 +0600

    Add files via upload

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+import string
+import random
 # Create your models here.
+def generate_unique_code():
+    length = 6
+    
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+        if not Room.objects.filter(code=code).exists():
+            break
+    return code
+    
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False,default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    #put most of the logic in models.py
+    
+    
